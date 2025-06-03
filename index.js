1a1e7d208704ad4decae9e44f53048df781213dd
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-06-03 10:13:52 +0300

    Used DOM to add backgroundcolor and elements on a list

--- index.js
+++ index.js
@@ -11,15 +11,42 @@ heading3.forEach(h3 => {
 });
 
 const fruitsList = document.getElementById('fruList');
+const orange=document.createElement('img')
+orange.src= 'imaes/oranges.jpeg';
+    orange.style.width = '10vw';
+     orange.style.height = '20vh';
+     orange.style.borderRadius = '500px';
+fruitsList.appendChild(orange)
 const newFruit = document.createElement('li');
 newFruit.textContent = 'Orange'; 
+newFruit.style.marginLeft='-18vw'
+newFruit.style.marginTop='25vh'
 fruitsList.appendChild(newFruit);
+const priceFruit=document.createElement('p');
+priceFruit.textContent='60 KSH per KILOS'
+priceFruit.style.marginLeft='-15vw'
+priceFruit.style.marginTop='28vh'
+fruitsList.appendChild(priceFruit)
+
 
 
 const vegetablesList = document.getElementById('vegList');
+const spinach=document.createElement('img')
+spinach.src= 'imaes/spinach.jpeg';
+    spinach.style.width = '10vw';
+    spinach.style.height = '20vh';
+    spinach.style.borderRadius = '500px';
+    vegetablesList.appendChild(spinach)
 const newVegetable = document.createElement('li');
 newVegetable.textContent = 'Spinach'; 
+newVegetable.style.marginLeft='-18vw'
+newVegetable.style.marginTop='25vh'
 vegetablesList.appendChild(newVegetable);
+const priceVege=document.createElement('p');
+priceVege.textContent='70 KSH per KILOS'
+priceVege.style.marginLeft='-15vw'
+priceVege.style.marginTop='28vh'
+vegetablesList.appendChild(priceVege)
 
 
 
